d06a5b2939cd413258145113b3d7b4d3c1a1ab63
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-19 20:47:02 +0530

    ADDED: junit test method to test writing data to a file

---
+++ lib/src/test/java/com/bridgelabz/payroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.payroll;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.*;
+import static com.bridgelabz.payroll.EmployeePayrollService.IOService.FILE_IO;
+public class EmployeePayrollServiceTest {
+
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = {
+				new EmployeePayrollData(1, "Jeff Bezos", 10000),
+				new EmployeePayrollData(2, "Bill Gates", 20000),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 30000)
+		};
+		
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(FILE_IO);
+		employeePayrollService.printData(FILE_IO);
+		long entries = employeePayrollService.countEntries(FILE_IO);
+		Assert.assertEquals(3,entries);
+	}
+}
