99b875c2352bc0b4ee82102409c22124eb5ce843
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-19 20:15:32 +0530

    ADDED: junit test method to test watch service

--- lib/src/test/java/com/bridgelabz/payroll/NIOfileAPITest.java
+++ lib/src/test/java/com/bridgelabz/payroll/NIOfileAPITest.java
@@ -39,4 +39,12 @@ public class NIOfileAPITest {
 		
 	}
 	
+
+    @Test
+    public void givenAsDirectoryWhenWatchedListsAllTheActivities() throws IOException {
+    	Path dir = Paths.get(HOME+"/"+PLAY_WITH_NIO);
+    	Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+    	new Java8WatchServiceExample(dir).processEvents();
+    }
+	
 }
