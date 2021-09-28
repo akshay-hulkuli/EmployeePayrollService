db92533d6e6dabe461b489a8cc042417b6e383bc
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-28 23:10:38 +0530

    ADDED: class that holds junit test methods to test fileIO

---
+++ lib/src/test/java/com/bridgelabz/payroll/EmployeePayrollServiceFileIOTest.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.payroll;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import static com.bridgelabz.payroll.EmployeePayrollService.IOService.*;
+public class EmployeePayrollServiceFileIOTest {
+	static int  size = 3;
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = {
+				new EmployeePayrollData(1, "Jeff Bezos", 10000),
+				new EmployeePayrollData(2, "Bill Gates", 20000),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 30000)
+		};
+		
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(FILE_IO);
+		employeePayrollService.printData(FILE_IO);
+		long entries = employeePayrollService.countEntries(FILE_IO);
+		Assert.assertEquals(3,entries);
+	}
+	
+	@Test
+	public void givenFileOnReadingFromMatchEmployeeCount() {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		long entries = employeePayrollService.readEmployeePayrollData(FILE_IO);
+		Assert.assertEquals(3,entries);
+	}
+	
+	
+	
+}
